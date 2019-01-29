6c8830c2b082bb86d446953499ad1ab1ec8314ae
Tanish Charaya <charayatanish@gmail.com>
2019-01-29 23:58:58 +0530

    Add GET /users/me and authentiction middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,6 +45,22 @@ UserSchema.methods.generateAuthToken=function(){
 		return token;
 	});
 };
+UserSchema.statics.findByToken=function(token){
+	var User=this;
+	var decoded;
+	try{
+		decoded=jwt.verify(token,'123abc')
+	}
+	catch (e){
+
+	}
+
+	return User.findOne({
+		'_id':decoded.id,
+		'tokens.token':token,
+		'tokens.access':'auth'
+	});
+}
 var User=mongoose.model('User',UserSchema);
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-
+var {authenticate}=require('./middleware/authenticate');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -95,6 +95,11 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+});
 app.post('/users',(req,res)=>{
   var body=_.pick(req.body,['email','password']);
   var user = new User(body);
